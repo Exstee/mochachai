ed2d60cc7568c5d481aa882fde8ff02d709e8700
Exstee <exsteeyo@gmail.com>
2025-10-20 21:55:03 -0500

    Fix Functional Test #5 - validate form submission for Vespucci

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -97,9 +97,14 @@ suite('Functional Tests with Zombie.js', function () {
     });
     // #6
     test('Submit the surname "Vespucci" in the HTML form', function (done) {
-      assert.fail();
-
+      browser.fill('surname', 'Vespucci');
+      browser.pressButton('submit', function () {
+        browser.assert.success();
+        browser.assert.text('span#name', 'Amerigo');
+        browser.assert.text('span#surname', 'Vespucci');
+        browser.assert.elements('span#dates', 1);
       done();
+      });
     });
   });
 });
